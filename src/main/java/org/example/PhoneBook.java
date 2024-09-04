bec42285eef29ce2cc9d1e8eddc41b8d81e6f63b
Arti <artmasterae@mail.ru>
2024-09-04 12:44:40 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -4,9 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
+    public Map<String, Integer> book = new HashMap<>();
+    public int count = 0;
 
     public int add(String name, Integer number) {
-
-        return 0;
+        if (!book.containsKey(name)) {
+            book.put(name, number);
+            count++;
+        }
+        return count;
     }
 }
