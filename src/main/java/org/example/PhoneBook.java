ea378b4636eb56c4a5f875b1b81739eedeb298ed
Arti <artmasterae@mail.ru>
2024-09-04 12:40:06 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    public int add(String name, Integer number) {
+
+        return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,4 +1,16 @@
 package org.example;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    public void addTest() {
+        String name = "Artem";
+        Integer number = 2345;
+        int result = phoneBook.add(name, number);
+        Assertions.assertEquals(result, 1);
+    }
+
 }
